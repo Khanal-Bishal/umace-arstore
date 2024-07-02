fbd4aa9efc659461f80e839eb38439c36bef8c4c
Bishal <Bishaldada777@gmail.com>
2024-07-02 11:22:16 +0545

    feat/style: Add new AR store logo and design home route to be responsive

--- admin2/src/sections/blog/post-card.jsx
+++ admin2/src/sections/blog/post-card.jsx
@@ -39,7 +39,7 @@ export default function PostCard({ post, index }) {
           zIndex: 9,
           top: 24,
           left: 24,
-          width: 40,
+        width: 40,
           height: 40,
         }),
       }}
